7a67ad9dff232b8202e243ba48ffca2e1d6ec9c8
Hannah Cussen <hcussen6@gmail.com>
2022-11-06 14:29:48 -0800

    began simulation objects draft

---
+++ code/axon.py
@@ -0,0 +1,5 @@
+class Axon:
+    def __init__(self) -> None:
+        pass
+    
+    
---
+++ code/environment.py
@@ -0,0 +1,21 @@
+class GridSquare:
+    def __init__(self) -> None:
+        self.netrin = 0
+        self.shh = 0
+        self.slit = 0
+        self.hasAxonShaft = False
+
+
+
+class Environment:
+    def __init__(self) -> None:
+        self.nrows = 2
+        self.ncols = 100
+        self.grid = []
+    
+    def calibrateGrid(self):
+        pass
+
+        
+
+        

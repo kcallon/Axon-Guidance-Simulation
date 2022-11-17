21bd9a50a1da1a13bc818522068238ebebab8ea2
Hannah Cussen <hcussen6@gmail.com>
2022-11-16 17:20:11 -0800

    making sim.py have a main function

--- code/sim.py
+++ code/sim.py
@@ -1,7 +1,19 @@
 from axon import Axon
 from environment import Environment
 
-env = Environment()
+def main():
+    env = Environment()
 
-env.calibrateGrid()
-env.print(midline=True, conc=True)
+    env.calibrateGrid()
+    env.print(midline=True)
+
+    for j in range(env.ncols - 3):
+        square = env.getGridSquare(1, j)
+        square.hasAxonShaft = True
+    square = env.getGridSquare(0, )
+    square.hasAxonShaft = True
+    print()
+    env.print(midline=True)
+
+if __name__ == "main":
+    main()

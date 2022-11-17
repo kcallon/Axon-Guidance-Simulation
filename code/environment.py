c14eb4a4504213075fa8db19ad5da32f439ff18f
Hannah Cussen <hcussen6@gmail.com>
2022-11-16 16:31:32 -0800

    fixing shallow copy in env gridSquare, adding getGridSquare method

--- code/environment.py
+++ code/environment.py
@@ -1,4 +1,6 @@
 from math import floor
+from axon import Axon
+
 class GridSquare:
     def __init__(self) -> None:
         self.netrin = 0
@@ -11,8 +13,9 @@ class Environment:
         self.geneConfig = {
             'netrin': True,
             'slit': True,
-            'SHH': True,
+            'shh': True,
         }
+        
         self.middle = 0.2 #the fraction of cells considered the midline
         self.nrows = 2
         self.ncols = 10
@@ -27,33 +30,33 @@ class Environment:
 
     def calibrateGrid(self):
         def concentration(a, b, x ):
-            print(a, b, x)
-            print(f'({a[1] - b[1]}) / ({a[0] - b[0]})')
             return (a[1] - b[1]) / (a[0] - b[0]) * x
 
-        row = []
-        for j in range(self.ncols):
-            square = GridSquare()
-            if self.inMidline(0, j) == "left":
-                left_edge = (0, 0) # (column, concentration) tuple
-                right_edge = ((0.5 - (self.middle / 2)) * self.ncols, self.initialConcentrations['netrin'])
-                square.netrin = concentration(left_edge, right_edge, j)
-
-                right_edge = ((0.5 - (self.middle / 2)) * self.ncols, self.initialConcentrations['shh'])
-                square.shh = concentration(left_edge, right_edge, j)
-
-            elif self.inMidline(0, j) == "middle":
-                square.slit = self.initialConcentrations['slit']
-                square.netrin = self.initialConcentrations['netrin']
-                square.shh = self.initialConcentrations['shh']
-            else:
-                pass
-
-            row.append(square)
         for i in range(self.nrows):
+            row = []
+            for j in range(self.ncols):
+                square = GridSquare()
+                if self.inMidline(0, j) == "left":
+                    left_edge = (0, 0) # (column, concentration) tuple
+                    right_edge = ((0.5 - (self.middle / 2)) * self.ncols, self.initialConcentrations['netrin'])
+                    square.netrin = concentration(left_edge, right_edge, j)
+
+                    right_edge = ((0.5 - (self.middle / 2)) * self.ncols, self.initialConcentrations['shh'])
+                    square.shh = concentration(left_edge, right_edge, j)
+
+                elif self.inMidline(0, j) == "middle":
+                    square.slit = self.initialConcentrations['slit']
+                    square.netrin = self.initialConcentrations['netrin']
+                    square.shh = self.initialConcentrations['shh']
+                else:
+                    pass
+
+                row.append(square)
             self.grid.append(row)
             
-
+    def getGridSquare(self, x, y) -> GridSquare:
+        return self.grid[x][y]
+    
     def inMidline(self, x, y) -> str:
         """Check if an x, y cell is in the midline
         Midline is the middle 20% of the Environment
@@ -96,7 +99,6 @@ class Environment:
                     r.insert(floor((0.5 - (self.middle / 2)) * self.ncols), '|')
                     r.insert(floor((0.5 + (self.middle / 2)) * self.ncols) + 1, '|')
                 print(''.join(r))
-
 
 
 

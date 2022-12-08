527a0ad21813eefec9d1d38f78fb22d590af9518
Hannah Cussen <hcussen6@gmail.com>
2022-12-07 20:07:12 -0800

    running sims with updated roundabout

--- code/run_sims_compare_output.py
+++ code/run_sims_compare_output.py
@@ -2,9 +2,9 @@ import utils
 import os
 
 def main():
-    inputdir = '../configuration_inputs'
-    outputdir = '../configuration_outputs'
-    exampledir = '../examples'
+    inputdir = 'configuration_inputs'
+    outputdir = 'configuration_outputs'
+    exampledir = 'examples'
     logfile = f'{outputdir}/log.txt'
     configs = os.listdir(inputdir)
     os.makedirs(outputdir, exist_ok=True)
@@ -15,7 +15,7 @@ def main():
             inputfile = f'{inputdir}/{config}'
             outputfile = f'{outputdir}/{name}'
             print(f'running sim on {inputfile} with name {name}')
-            os.system(f'python sim.py {inputfile} > {outputfile}')
+            os.system(f'python code/sim.py {inputfile} > {outputfile}')
 
             edit_dist = utils.edit_distance(f'{exampledir}/{name}', outputfile)
             log.write(f'{name}\t{edit_dist}\n')
